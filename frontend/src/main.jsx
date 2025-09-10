a9c38e77184b90488d1ebc31e074b18c4dfd49a4
Ileuza Maya <artemoldurados@gmail.com>
2025-09-10 06:45:37 -0300

    feat(frontend): atualizações locais

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./App.css";
-import AppRoutes from "./routes/AppRoutes.jsx";
+import AppRoutes from "./AppRoutes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
