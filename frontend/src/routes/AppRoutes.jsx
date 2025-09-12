1cf06c3a5b5bf1361b7bb61e81339e12193b23fd
Ileuza Maya <artemoldurados@gmail.com>
2025-09-12 15:21:38 -0300

    feat(frontend): atualizações locais

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,15 +1,15 @@
 // frontend/src/routes/AppRoutes.jsx
-
 import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
 import OrcamentoForm from "../pages/Orcamento.jsx";
 import AuthSplit from "../components/AuthSplit.jsx";
 import ResetPassword from "../pages/ResetPassword.jsx";
 import { useAuth } from "../auth/AuthContext.jsx";
+import Admin from "../pages/Admin.jsx";          // <- importe aqui
 
 function RequireAuth() {
   const { user, loading } = useAuth();
   const location = useLocation();
-  if (loading) return null; // pode pôr um spinner
+  if (loading) return null; // pode renderizar spinner se quiser
   if (!user) {
     const next = encodeURIComponent(location.pathname + location.search);
     return <Navigate to={`/?next=${next}`} replace />;
@@ -27,9 +27,10 @@ export default function AppRoutes() {
       {/* protegidas */}
       <Route element={<RequireAuth />}>
         <Route path="/orcamento" element={<OrcamentoForm />} />
+        <Route path="/admin" element={<Admin />} />      {/* <- AQUI */}
       </Route>
 
-      {/* catch-all */}
+      {/* fallback */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
