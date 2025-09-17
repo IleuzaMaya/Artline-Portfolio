35281c3a9be2923bc46194914a0d278ccca716d2
Ileuza Maya <artemoldurados@gmail.com>
2025-09-17 06:50:48 -0300

    fix(login): usar AuthSplit como única tela

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { Link } from "react-router-dom";
 import "./index.css";
 import "./App.css";
 
